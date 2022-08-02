da942a232ef625f5f1ecd5d44109eb5a141aa485
PiotrAndrzejewsky <piotrandrzejewski@onet.eu>
2022-08-02 19:29:33 +0200

    fix: exclude data source auto configuration

--- src/main/java/com/example/Task4/Task4Application.java
+++ src/main/java/com/example/Task4/Task4Application.java
@@ -2,8 +2,9 @@ package com.example.Task4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Task4Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Task4Application.class, args);
